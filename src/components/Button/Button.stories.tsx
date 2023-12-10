7daa8ac3d052fbb28478b48672c7705f8b08729a
IvanovViktor1 <uhmah-mar@yandex.ru>
2023-12-10 21:53:41 +0500

    Создал компонент кнопки

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -11,13 +11,10 @@ export default {
 // больше про шаблоны компонента: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: StoryFn<typeof Button> = (args) => <Button {...args} />;
 
-export const HelloWorld = Template.bind({});
-HelloWorld.args = {
-    content: 'Hello world'
-}
-// больше про args: https://storybook.js.org/docs/react/writing-stories/args
-
-export const ClickMe = Template.bind({});
-ClickMe.args = {
-  content: "Click me!",
+export const Demo = Template.bind({});
+Demo.args = {
+  content: "Demo button",
+  type: "main",
+  size: "medium",
 };
+// больше про args: https://storybook.js.org/docs/react/writing-stories/args
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,14 +1,49 @@
 import React, { FC } from "react";
-
-import "./styles.css"
+import classNames from "classnames";
+import { ButtonIcons } from "./Icons";
+import "./styles.css";
 
 export type ButtonProps = {
-    content: React.ReactNode;
-}
+  backgroundColor?: string;
+  content: React.ReactNode;
+  icon?: "add" | "remove" | "edit" | "copy" | "settings" | "like";
+  type?: "main" | "secondary";
+  size?: "large" | "medium" | "small";
+  disabled?: boolean;
+  onClick?: () => void;
+};
 
-export const Button: FC<ButtonProps> = (props) => {
-    
-    return (
-        <button className="my-button">{props.content}</button>
-    )
-}
+export const Button: FC<ButtonProps> = ({
+  backgroundColor,
+  content,
+  icon,
+  type,
+  size,
+  disabled,
+  onClick,
+}) => {
+  const btnClass = classNames(
+    "my-button",
+    backgroundColor ? `color-${backgroundColor.replace("#", "")}` : "",
+    type,
+    size,
+    { disabled }
+  );
+
+  const btnIcon = () => {
+    if (icon) {
+      return ButtonIcons.find((bIcon) => bIcon.nameSvg === icon)?.svg(size);
+    } else return <></>;
+  };
+
+  return (
+    <button
+      className={btnClass}
+      onClick={onClick}
+      style={{ backgroundColor: backgroundColor }}
+    >
+      {btnIcon()}
+      {content}
+    </button>
+  );
+};
